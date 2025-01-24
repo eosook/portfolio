4a6fe49aa2b6c87c82c835a50d0bb1aa0210f469
Eo Soo Kim <eosookim@gmail.com>
2025-01-23 22:24:16 -0500

    simple placeholder for the about/main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 import "./App.scss";
 import Header from "./components/Header/Header";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import Skills from "./components/Skills/Skills"
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
     <div class="portfolio">
       <Header />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
     </div>
   );
 }
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,36 @@
-import './About.scss'
+import "./About.scss";
+import aboutImage from "../../assets/images/Cozy Warming Pad.png";
 
-export default function About(){
-    return (
-        <div className="about">
-            
-        </div>
-    )
+export default function About() {
+  function sendTo(url) {
+    const newWindow = window.open(url, "_blank", "noopener,noreferrer");
+    if (newWindow) {
+      newWindow.opener = null;
+    }
+  }
+  return (
+    <div className="about">
+      <div className="about__container">
+        <img src={aboutImage} alt="" className="about__image" />
+      </div>
+      <div className="about__container">
+        <h1 className="about__title">Eo Soo Kim</h1>
+        <h2 className="about__subtitle">Software Engineer</h2>
+        <p className="about__body">Hello, i like game</p>
+        <button
+          className="about__button"
+          onClick={() => sendTo("https://github.com/eosook")}
+        >
+          Github
+        </button>
+        <button
+          className="about__button"
+          onClick={() => sendTo("https://www.linkedin.com/in/eosoo/")}
+        >
+          LinkedIn
+        </button>
+        <button className="about__button">Let's Talk</button>
+      </div>
+    </div>
+  );
 }
