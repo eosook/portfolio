b8175d6700a2726c3a1999b5eca05af7b43bd865
Eo Soo Kim <eosookim@gmail.com>
2025-02-10 21:24:20 -0500

    styling for project card

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,47 +1,116 @@
 import "./ProjectCard.scss";
-import cilesiaLogo from "../../assets/images/cilesiaLogo.png";
-import cilesiaImage from "../../assets/images/Cilesia Website.png";
-import gameRecImage from "../../assets/images/GameRec.png";
-import invoiceImage from "../../assets/images/Invoice App Dark.png";
+import leftArrow from "../../assets/images/left-arrow.png";
+import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
-import { motion } from "framer-motion";
 
-export default function ProjectCard({ projectNumber, closeWork, changeWork, selectedWork, description, logo, image }) {
+const cardVariants = {
+  expanded: { width: "100vw", height: "600px", borderRadius: 16 },
+  collapsed: { width: "300px", height: "500px", borderRadius: 16 },
+};
+
+export default function ProjectCard({
+  projectNumber,
+  title,
+  closeWork,
+  changeWork,
+  selectedWork,
+  description,
+  logo,
+  image,
+  techs,
+  link,
+}) {
+  const [showOtherCards, setShowOtherCards] = useState(true);
+
   return (
-    <motion.div
-      className={"project-card"}
-      layout
-      onClick={() => {
-        selectedWork == projectNumber ? closeWork() : changeWork(projectNumber);
-      }}
-      initial={{ borderRadius: 16 }}
-      animate={{
-        width: selectedWork == projectNumber ? "100vw" : "300px",
-        height: selectedWork == projectNumber ? "500px" : "400px",
-        borderRadius: selectedWork == projectNumber ? 0 : 16,
-      }}
-      exit={{}}
-      transition={{ duration: 0.5, ease: "easeIn" }}
-    >
-      <button
-        className="project-card__button"
-        onClick={(event) => {
-          event.stopPropagation();
-          closeWork();
-        }}
-      >
-        X
-      </button>
-      <img
-        className="project-card__logo"
-        src={logo}
-        alt="cilesia beauty bar logo"
-      ></img>
-      <img
-        className="project-card__image"
-        src={image}
-        alt="cilesia website image"
-      ></img>
-    </motion.div>
+    <AnimatePresence>
+      {selectedWork == 0 || selectedWork == projectNumber ? (
+        <motion.div
+          key={projectNumber}
+          className={
+            selectedWork == projectNumber
+              ? "project-card project-card__expanded"
+              : "project-card"
+          }
+          layout
+          onClick={() => {
+            if (selectedWork == projectNumber) {
+              setShowOtherCards(false);
+              closeWork();
+            } else {
+              changeWork(projectNumber);
+            }
+          }}
+          variants={cardVariants}
+          initial={{ borderRadius: 16 }}
+          animate={selectedWork == projectNumber ? "expanded" : "collapsed"}
+          exit={{}}
+          transition={{ duration: 0.4, ease: "easeIn" }}
+          onAnimationComplete={() => {
+            if (selectedWork !== projectNumber) {
+              setShowOtherCards(true);
+            }
+          }}
+          whileHover={
+            selectedWork !== projectNumber
+              ? {
+                  scale: 1.05,
+                  transition: { duration: 0.1 },
+                  border: "1px solid black",
+                }
+              : ""
+          }
+          whileTap={{ scale: 0.9 }}
+        >
+          {selectedWork == projectNumber ? (
+            <div className="project-card__container">
+              <img
+                className="project-card__back-arrow"
+                src={leftArrow}
+                onClick={(event) => {
+                  event.stopPropagation();
+                  closeWork();
+                }}
+              ></img>
+              <div className="project-card__content">
+                <div>
+                  <img
+                    className="project-card__logo"
+                    src={logo}
+                    alt="cilesia beauty bar logo"
+                  ></img>
+                  <h2 className="project-card__title">{title}</h2>
+                </div>
+                <p className="project-card__description">{description}</p>
+              </div>
+              <div className="project-card__techs">
+                {techs.map((tech) => {
+                  return (
+                    <div className="project-card__techs--item">
+                      {tech}
+                    </div>
+                  )
+                })}
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
+          <img
+            className={selectedWork == projectNumber ? "project-card__image project-card__image-expanded" : "project-card__image"}
+            src={image}
+            alt="cilesia website image"
+          ></img>
+        </motion.div>
+      ) : showOtherCards ? (
+        <motion.div
+          key={projectNumber}
+          className="project-card"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+        ></motion.div>
+      ) : null}
+    </AnimatePresence>
   );
 }
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -23,6 +23,7 @@ export default function Projects() {
       <div className="projects__section">
         <ProjectCard
           projectNumber={1}
+          title={"Cilesia Beauty Bar"}
           changeWork={changeWork}
           closeWork={closeWork}
           selectedWork={selectedWork}
@@ -31,9 +32,12 @@ export default function Projects() {
           }
           logo={cilesiaLogo}
           image={cilesiaImage}
+          techs={["React.js", "Email Integration", "Responsive Design"]}
+          link={"https://cilesiabeautybar.com/"}
         />
         <ProjectCard
           projectNumber={2}
+          title={"GameRec"}
           changeWork={changeWork}
           closeWork={closeWork}
           selectedWork={selectedWork}
@@ -42,9 +46,18 @@ export default function Projects() {
           }
           logo={cilesiaLogo}
           image={gameRecImage}
+          techs={[
+            "React.js",
+            "Node.js",
+            "IGDB API",
+            "SQL",
+            "Full-Stack Development",
+          ]}
+          link={"https://cilesiabeautybar.com/"}
         />
         <ProjectCard
           projectNumber={3}
+          title={"Invoice Tracker"}
           changeWork={changeWork}
           closeWork={closeWork}
           selectedWork={selectedWork}
@@ -53,44 +66,34 @@ export default function Projects() {
           }
           logo={cilesiaLogo}
           image={invoiceImage}
+          techs={[
+            "React.js",
+            "TypeScript",
+            "Node.js",
+            "Full-Stack Development",
+          ]}
+          link={"https://cilesiabeautybar.com/"}
+        />
+        <ProjectCard
+          projectNumber={4}
+          title={"Invoice Tracker"}
+          changeWork={changeWork}
+          closeWork={closeWork}
+          selectedWork={selectedWork}
+          description={
+            "I built this full-stack web application as a hands-on project to learn and apply TypeScript in a real-world scenario. The platform allows users to efficiently manage invoices by adding, editing, and deleting them while maintaining data consistency and type safety throughout the application."
+          }
+          logo={cilesiaLogo}
+          image={invoiceImage}
+          techs={[
+            "React.js",
+            "TypeScript",
+            "Node.js",
+            "Full-Stack Development",
+          ]}
+          link={"https://cilesiabeautybar.com/"}
         />
       </div>
-      {/* <Project
-        image={cilesiaImage}
-        title={"Cilesia Beauty Bar"}
-        description={
-          "I designed and developed a modern, user-friendly website for a small beauty business specializing in lash services. Collaborating closely with the client, I crafted a custom design that aligns with their brand identity while ensuring functionality for both users and the business owner."
-        }
-        siteUrl={"https://cilesiabeautybar.com"}
-        techs={["React.js", "Email Integration", "Responsive Design"]}
-        side={"right"}
-      />
-      <Project
-        image={gameRecImage}
-        title={"GameRec"}
-        description={
-          "I developed a full-stack web application designed to help users discover new games tailored to their preferences. The platform integrates with the IGDB API from Twitch to fetch detailed game information, while users can create personalized accounts, maintain wishlists, and explore game recommendations."
-        }
-        siteUrl={"https://cilesiabeautybar.com"}
-        techs={[
-          "React.js",
-          "Node.js",
-          "IGDB API",
-          "SQL",
-          "Full-Stack Development",
-        ]}
-        side={"left"}
-      />
-      <Project
-        image={invoiceImage}
-        title={"Invoice Tracker"}
-        description={
-          "I built this full-stack web application as a hands-on project to learn and apply TypeScript in a real-world scenario. The platform allows users to efficiently manage invoices by adding, editing, and deleting them while maintaining data consistency and type safety throughout the application."
-        }
-        siteUrl={"https://cilesiabeautybar.com"}
-        techs={["React.js", "TypeScript", "Node.js", "Full-Stack Development"]}
-        side={"right"}
-      /> */}
     </div>
   );
 }
