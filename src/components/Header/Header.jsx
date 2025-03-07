322a375eac701d302cb0e3e74f249c5a1ec0bae3
Eo Soo Kim <eosookim@gmail.com>
2025-03-06 22:00:23 -0500

    creates header dropdown menu for mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,10 +6,12 @@ import menuClose from "../../assets/images/menu-close.png";
 import sun from "../../assets/images/sun.png";
 import moon from "../../assets/images/moon.png";
 import { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Header({ visible, darkMode, setDarkMode }) {
   const [hasScrolled, setHasScrolled] = useState(false);
   const [openMenu, setOpenMenu] = useState(false);
+  const headerList = ["home", "projects", "skills", "contact"];
 
   useEffect(() => {
     if (visible == false) {
@@ -27,6 +29,7 @@ export default function Header({ visible, darkMode, setDarkMode }) {
 
   const sendTo = (id) => {
     document.getElementById(id)?.scrollIntoView({ behavior: "smooth" });
+    setOpenMenu(false);
   };
 
   return (
@@ -43,9 +46,15 @@ export default function Header({ visible, darkMode, setDarkMode }) {
         <img src={logo} className="header__logo-image"></img>
       </div>
       <ul className="header__list">
-        <li className="header__list-home" onClick={() => sendTo("home")}>Home</li>
-        <li className="header__list-item" onClick={() => sendTo("projects")}>Projects</li>
-        <li className="header__list-item" onClick={() => sendTo("skills")}>Skills</li>
+        <li className="header__list-home" onClick={() => sendTo("home")}>
+          Home
+        </li>
+        <li className="header__list-item" onClick={() => sendTo("projects")}>
+          Projects
+        </li>
+        <li className="header__list-item" onClick={() => sendTo("skills")}>
+          Skills
+        </li>
       </ul>
       <div className="header__right-container">
         <div className="header__mode">
@@ -56,7 +65,12 @@ export default function Header({ visible, darkMode, setDarkMode }) {
           ></img>
         </div>
         <div className="header__contact">
-          <button className="header__contact-button" onClick={() => sendTo("contact")}>Contact</button>
+          <button
+            className="header__contact-button"
+            onClick={() => sendTo("contact")}
+          >
+            Contact
+          </button>
         </div>
         <div className="header__menu">
           <img
@@ -65,6 +79,36 @@ export default function Header({ visible, darkMode, setDarkMode }) {
             onClick={menuClick}
           ></img>
         </div>
+        <AnimatePresence>
+          {openMenu ? (
+            <motion.div
+              className="header__dropdown-menu"
+              initial={{ scale: 1 }}
+              animate={{
+                scale: 1.4,
+                transition: { type: "spring", bounce: 0.6, duration: 0.4 },
+              }}
+              exit={{ scale: 1 }}
+            >
+              <ul className="header__dropdown-list">
+                <div className="header__dropdown-list-arrow"></div>
+                {headerList.map((item) => (
+                  <motion.li
+                    key={item}
+                    className="header__dropdown-list-item"
+                    onClick={() => sendTo(item)}
+                    whileHover={{ scale: 1.1, color: "white" }}
+                    transition={{ type: "spring", stiffness: 400 }}
+                  >
+                    {item.charAt(0).toUpperCase() + item.slice(1)}
+                  </motion.li>
+                ))}
+              </ul>
+            </motion.div>
+          ) : (
+            ""
+          )}
+        </AnimatePresence>
       </div>
     </div>
   );
